902c637dda19b093181421ddac155f62c9165ca0
202145225 최유진 <154939459+202145225@users.noreply.github.com>
2023-12-28 12:21:08 +0900

    Add files via upload
    #VSCode

---
+++ 231228.py
@@ -0,0 +1,29 @@
+#VSCode 에서 작성
+print("Hello")
+print("python")
+
+#2-1 함수로 화면에 출력하기
+print('부산대학교')
+print('기초교육원')
+print('컴퓨팅적 사고와 코딩')
+
+#2-2 (double_quotes.py)
+print("부산대학교")
+print("기초교육원")
+print("컴퓨팅적 사고와 코딩")
+
+#2-3(display_quote.py)
+print("""One
+Two
+Three""")
+
+#2-4 주석활용하기(Commnets)
+#화면에 여러 줄을
+#출력하는 방법에 대한 코드입니다. print("부산대학교")
+print("기초교육원")
+print("컴퓨팅적 사고와 코딩")
+
+#2-5 (commnet2.py)
+print('부산대학교') #첫 번째 줄에 ‘부산대학교’를 출력합니다. 
+print('기초교육원') #두 번째 줄에 ‘기초교육원’을 출력합니다. 
+print('컴퓨팅적 사고와 코딩') #세 번째 줄에 ‘파이썬 프로그래밍 기초’를 출력합니다.
